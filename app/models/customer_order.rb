ba9e618839f2eb924e530a4754098066b3442493
Suresh Podeti <spodeti@cigital.com>
2016-12-04 19:35:38 +0530

    upload images to s3 amazon via paperclip

--- app/models/customer_order.rb
+++ app/models/customer_order.rb
@@ -3,7 +3,7 @@ class CustomerOrder < ApplicationRecord
 	has_one :delivery_address
 	has_one :customer_payment
 	has_one :order_status
-	has_attached_file :artifact, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :artifact, styles: { medium: "300x300>", thumb: "100x100>", square: '200x200#' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :artifact, content_type: /^image\/(jpg|jpeg|png|pdf)$/, :message => 'file type is not allowed (only jpeg/png/pdf images)'
   validates_presence_of :description, :quantity, :expected_delivery_date
 end
